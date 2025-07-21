ee17691798342d4eef1ded9886d35bbbd96144b5
Eason Chang <eason@easonchang.com>
2025-07-21 00:14:28 -0600

    Fix formatting in useProductHuntData.ts

--- src/hooks/useProductHuntData.ts
+++ src/hooks/useProductHuntData.ts
@@ -1,7 +1,7 @@
 export default function useProductHuntData() {
   // Use environment variable to determine if this is a midday post
   const isMiddayPost = process.env.MIDDAY_POST === "true";
-  
+
   // Import the correct data file
   let data: {
     date: string;
